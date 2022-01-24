b2198edbc8332e56de1b2383d3aaaef8c5297cd4
Mohamed Elgendy <54114536+M-R-Elgendy@users.noreply.github.com>
2022-01-25 00:23:44 +0200

    main

---
+++ asset/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    AOS.init();
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $(window).height()) {
+            $("#mainNav").removeClass("bg-transparent navbar-dark").addClass(
+                "bg-light navbar-light");
+        } else {
+            $("#mainNav").addClass("bg-transparent navbar-dark").removeClass(
+                "bg-light navbar-light");
+        }
+
+    })
+})
