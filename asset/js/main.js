d399067abef80c7cafe843ce9ff7294691d52eee
Mohamed Elgendy <54114536+M-R-Elgendy@users.noreply.github.com>
2022-01-25 00:40:29 +0200

    Linconwaves

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,7 +4,7 @@ $(function () {
 
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() > $(window).height()) {
+        if ($(window).scrollTop() > $(window).height() - 500) {
             $("#mainNav").removeClass("bg-transparent navbar-dark").addClass(
                 "bg-light navbar-light");
         } else {
