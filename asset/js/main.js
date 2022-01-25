349a4fb2d68598f476bfc89454ce41489e91f2dd
Mohamed Elgendy <54114536+M-R-Elgendy@users.noreply.github.com>
2022-01-26 00:15:01 +0200

    Header UI Update

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,10 +5,10 @@ $(function () {
 $(document).ready(function () {
     $(window).scroll(function () {
         if ($(window).scrollTop() > $(window).height() - 500) {
-            $("#mainNav").removeClass("bg-transparent navbar-dark").addClass(
+            $("#mainNav").removeClass("bg-transparent navbar-dark nav-border").addClass(
                 "bg-light navbar-light");
         } else {
-            $("#mainNav").addClass("bg-transparent navbar-dark").removeClass(
+            $("#mainNav").addClass("bg-transparent navbar-dark nav-border").removeClass(
                 "bg-light navbar-light");
         }
 
